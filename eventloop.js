719f429d06ade31332acb178172a8f45d1e4a93e
Am1d1v <dimadiman1adima@gmail.com>
2024-02-11 16:53:12 +0300

    Event Loop

--- eventloop.js
+++ eventloop.js
@@ -5,7 +5,7 @@ console.log('Init');
 
 setTimeout(() => {
     console.log(performance.now(), 'Timer 0')
-}, 10);
+}, 100);
 
 setImmediate(() => {
     console.log('Immediate');
@@ -19,11 +19,24 @@ setTimeout(() => {
     for(let i = 0; i < 1000000000; i++){
         
     }
-    console.log('Big Operation Done')
+    console.log('Big Operation Done');
+
+    Promise.resolve().then(() => {
+        console.log('Promise inside timeout');
+    });
+
+    process.nextTick(() => {
+        console.log('Tick inside timeout');
+    });
+    
 }, 100);
 
 Promise.resolve().then(() => {
     console.log('Promise')
+})
+
+process.nextTick(() => {
+    console.log('Tick');
 })
 
 console.log('Final');
