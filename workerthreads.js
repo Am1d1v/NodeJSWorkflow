2913aa5c2cbdcd75200c379bf2ba67d4c107b505
Am1d1v <dimadiman1adima@gmail.com>
2024-02-12 14:57:23 +0300

    Worker Threads

---
+++ workerthreads.js
@@ -0,0 +1,10 @@
+
+
+const crypto = require('crypto');
+const start = performance.now();
+
+for(let i = 0; i < 50; i++){
+    crypto.pbkdf2('test', 'salt', 100, 64, 'sha512', () => {
+        console.log(performance.now() - start);
+    });
+}
