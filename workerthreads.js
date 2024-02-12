d42a90b4edfcbfd580ab3e1e0551998b5d139e6a
Am1d1v <dimadiman1adima@gmail.com>
2024-02-12 15:05:36 +0300

    Worker Threads. Increase value of thread pool.

--- workerthreads.js
+++ workerthreads.js
@@ -1,10 +1,30 @@
 
 
 const crypto = require('crypto');
+const https = require('https');
 const start = performance.now();
 
+// Increase thread pull, default value of thread pool is 4
+// process.env.UV_THREADPOOL_SIZE = 8;
+
+// for(let i = 0; i < 50; i++){
+//     crypto.pbkdf2('test', 'salt', 100, 64, 'sha512', () => {
+//         console.log(performance.now() - start);
+//     });
+// }
+
 for(let i = 0; i < 50; i++){
-    crypto.pbkdf2('test', 'salt', 100, 64, 'sha512', () => {
-        console.log(performance.now() - start);
-    });
+
+    https.get('https://yandex.ru', (res) => {
+
+        res.on('data', () => {
+            
+        });
+
+        res.on('end', () => {
+            console.log(performance.now() - start);
+        });
+
+    })
+
 }
