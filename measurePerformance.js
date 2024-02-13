a974191699b5c5067b090479b3721552374c4f38
Am1d1v <dimadiman1adima@gmail.com>
2024-02-13 13:31:04 +0300

    Performance Measurement

--- measurePerformance.js
+++ measurePerformance.js
@@ -1,10 +1,23 @@
 
 
-function slow(){
-    const arr = [];
 
+function slow(){
+    // Start performance mark
+    performance.mark('start');
+
+    const arr = [];
+   
     for (let i = 0; i < 1000000; i++){
         arr.push(i * i);
     }
+    
+    // End performance mark
+    performance.mark('end');
+
+    // Performance measure calculation
+    performance.measure('slow', 'start', 'end');
+    
+    console.log(performance.getEntriesByName('slow'));
 }
 slow();
+
